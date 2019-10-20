9d92d8bfc14633a1930e1fd16faef0b5e833dd18
Bryan Yee <bryan.yee@grandrounds.com>
2019-10-20 13:38:52 -0700

    Use static routing for posts

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 
 const PostLink = props => (
   <li>
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
+    <Link href={`/p/${props.id}`}>
       <a>{props.id}</a>
     </Link>
   </li>
@@ -15,9 +15,7 @@ export default function Blog() {
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink id="hello-nextjs" />
-        <PostLink id="learn-nextjs" />
-        <PostLink id="deploy-nextjs" />
+        <PostLink id="first-post" />
       </ul>
     </Layout>
   );
---
+++ pages/p/first-post.js
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <h1>First Post</h1>
+      <p>This is the first post.</p>
+    </Layout>
+  );
+}
