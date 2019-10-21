fcc3f016ce908b21daef7360234ba71b9e7d2b56
Bryan Yee <bryan.yee@grandrounds.com>
2019-10-20 18:48:43 -0700

    Comment out component

--- pages/p/a-visual-dive-into-react-fiber.js
+++ pages/p/a-visual-dive-into-react-fiber.js
@@ -2,12 +2,12 @@ import Layout from '../../components/Layout';
 
 import PAGES from '../../lib/pages';
 
-const page = PAGES.find(({ id }) => id = 'a-visual-dive-into-react-fiber');
+// const page = PAGES.find(({ id }) => id = 'a-visual-dive-into-react-fiber');
 
-export default function AVisualDiveIntoReactFiber() {
-  return (
-    <Layout date={page.date} title={page.title}>
-      <p>This is the first post.</p>
-    </Layout>
-  );
-}
+// export default function AVisualDiveIntoReactFiber() {
+//   return (
+//     <Layout date={page.date} title={page.title}>
+//       <p>This is the first post.</p>
+//     </Layout>
+//   );
+// }
