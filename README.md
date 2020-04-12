a71541cbf4e76497fda40379b68270edfb513639
Bryan Yee <bryan.yee@grandrounds.com>
2020-04-12 08:29:52 -0700

    Personalize project files generated by Gatsby

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A web software engineering blog.
+
+Bootstrapped with [Gatsby Default Starter](https://www.gatsbyjs.org/starters/gatsbyjs/gatsby-starter-default/).
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Engineer The Web`,
+    description: `A web software engineering blog.`,
+    author: `Bryan Yee`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
